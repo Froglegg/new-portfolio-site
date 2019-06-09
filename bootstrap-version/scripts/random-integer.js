d153a810f2f557f43b11aa42d9d6d399f062ad32
Froglegg <hayescrowley@gmail.com>
2019-06-09 14:44:22 -0400

    creating bootstrap version directory

---
+++ bootstrap-version/scripts/random-integer.js
@@ -0,0 +1,7 @@
+// get random integer
+function getRndInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+//console.log(getRndInt(0, 10));
+//<button onclick="document.getElementById('demo').innerHTML = getRndInteger(0,10)">Click Me</button>
