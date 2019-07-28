433cd33467a9f859cf93137df9580b2bb26393ef
Froglegg <hayescrowley@gmail.com>
2019-07-28 17:08:25 -0400

    removed bootstrap version

--- bootstrap-version/scripts/random-integer.js
+++
@@ -1,7 +0,0 @@
-// get random integer
-function getRndInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-
-//console.log(getRndInt(0, 10));
-//<button onclick="document.getElementById('demo').innerHTML = getRndInteger(0,10)">Click Me</button>
